a386fa12fde3ae19d19ecfab36488a27b2c738e8
Tamas Pentek <pentek_pityu@yahoo.com>
2021-01-13 21:00:01 +0000

    Update caregiver-container.js

--- src/caregiver/caregiver-container.js
+++ src/caregiver/caregiver-container.js
@@ -82,7 +82,9 @@ class CaregiverContainer extends React.Component {
 
     sendCurrentCaregiver = () => {
         console.log("SEND CURRENT CAREGIVER");
+        console.log("THIIIS: " + this.state.currentCaregiver.id);
         this.clientRef.sendMessage(HOST.backend_api + '/caregiverInfo', JSON.stringify({'caregiverID': this.state.currentCaregiver.id}));
+        console.log("SENTTT");
     }
 
 
