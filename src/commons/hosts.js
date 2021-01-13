4831cabce38c3267e8a1e13b244711d4030e9d85
Tamas Pentek <pentek_pityu@yahoo.com>
2021-01-13 21:58:53 +0000

    Update hosts.js

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -1,5 +1,6 @@
 export const HOST = {
     //backend_api: 'http://localhost:8080',
     //backend_api: 'https://springappbackenddemo.herokuapp.com/',
-    backend_api: 'https://springappbackenddocker2.herokuapp.com',
+    //backend_api: 'https://springappbackenddocker2.herokuapp.com',
+    backend_api: '/api',
 };
